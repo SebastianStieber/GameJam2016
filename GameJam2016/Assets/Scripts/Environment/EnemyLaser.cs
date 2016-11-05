6d604a4ad3f4a5f6de1175de1807b023a4277f8f
Georgi Petrov <gogo_93@gmx.de>
2016-11-05 16:08:25 +0100

    d

--- GameJam2016/Assets/Scripts/Environment/EnemyLaser.cs
+++ GameJam2016/Assets/Scripts/Environment/EnemyLaser.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class EnemyLaser : MonoBehaviour {
-
+	
+	public float timer;
 	public void EnemyAttack()
 
 	{
 		GameManager.instance.life--;
+		Destroy (gameObject);
+	}
+
+	void Update(){
+		timer -= Time.deltaTime;
+		if (timer<=0){
+			Destroy (gameObject);
+		}
 	}
 }
