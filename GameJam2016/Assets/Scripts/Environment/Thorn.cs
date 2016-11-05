f7aea6ab225e28945d31321cfed89dea5e9bbe68
Georgi Petrov <gogo_93@gmx.de>
2016-11-05 15:32:28 +0100

    g

--- GameJam2016/Assets/Scripts/Environment/Thorn.cs
+++ GameJam2016/Assets/Scripts/Environment/Thorn.cs
@@ -1,24 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
-=======
 
->>>>>>> 2521100b310ee81785854c85b2a15d9138bf8637
 
 public class Thorn : MonoBehaviour {
 
 
 	public void Die()
 	{
-<<<<<<< HEAD
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ().Restart ();
-		GameManager.instance.life--;
-
-=======
+		
 		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
 		GameManager.instance.life = 3;
->>>>>>> 2521100b310ee81785854c85b2a15d9138bf8637
+
 	}
 		
 }
