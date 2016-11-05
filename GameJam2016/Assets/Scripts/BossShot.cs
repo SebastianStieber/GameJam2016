570c77bf7e65efe6732b7899ba37b2c3041b9387
Sebastian Stieber <sstieber16@googlemail.com>
2016-11-05 18:50:16 +0100

    d

---
+++ GameJam2016/Assets/Scripts/BossShot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossShot : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.tag == "Player") {
+			Destroy (gameObject);
+			GameManager.instance.life--;
+		}
+	}
+}
